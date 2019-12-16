64a357fa60fc4366a59eda91a30f8eed7ffa8374
d-wojtucki <d.wojtucki@gmail.com>
2019-12-16 15:18:44 +0100

    Backend probably done

--- src/main/java/com/company/scrapper/controllers/MainController.java
+++ src/main/java/com/company/scrapper/controllers/MainController.java
@@ -6,16 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 
+@RequestMapping("/app")
 @Controller
 public class MainController {
 
     @Autowired
     AllegroApi allegroApi;
 
-    @GetMapping("/app")
+
+    //TODO main view - strona glowna z search boxem strzelajaca getem na /search z 3 parametrami - opisanie nizej
+    @GetMapping("/")
     public String home(
             Model model
     ) {
@@ -29,8 +34,9 @@ public class MainController {
         return "mainView";
     }
 
-    @GetMapping("/search")
-    public String search(
+    @Deprecated
+    @GetMapping("/search_test")
+    public String searchTest(
             Model model
     ) {
         ArrayList<Offer> offers = new ArrayList<Offer>();
@@ -44,5 +50,30 @@ public class MainController {
         model.addAttribute("offers", offers);
 
         return "mainView";
+    }
+
+    /*
+     * keywords - slowa kluczowe, po ktorych bedzie nastepowac wyszukiwanie, np "harry potter ksiazka"
+     * limit - ilosc wyswietlonych ofert
+     * sorting method - sortowanie ofert, domyslnie "+price" dla sortowania po cenie rosnaco
+     */
+    //TODO someview - widok listy itemow wraz z hiperlinkami budowanymi: allegro.pl/oferta/id
+    @GetMapping("/search")
+    public String search(
+            @ModelAttribute String keywords,
+            @ModelAttribute int limit,
+            @ModelAttribute String sortingMethod,
+            Model model
+            ) {
+        ArrayList<Offer> offers = new ArrayList<Offer>();
+        try {
+            offers = allegroApi.getListOfItemsWithKeywordsAsJson(keywords, limit, sortingMethod);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        for(Offer offer : offers) System.out.println(offer.toString());
+        model.addAttribute("offers", offers);
+        return "someView";
     }
 }
